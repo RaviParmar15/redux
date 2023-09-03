c05e967288e818773912c6f2bda83ebc3a18ccdd
RaviParmar <raviparmar78822@gmail.com>
2023-09-03 22:32:17 +0530

    redux  input count

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Add,Remove,Multi } from './Action';
+import { useState } from 'react';
 
 function App() {
+  let[number,setNumber]=useState(0)
   let count =useSelector((count) => count)
   let dispatch =useDispatch();
+  const HandleCount =()=>{
+    dispatch(Add(number))
+
+  }
   return (
     <div>
+      <input type='number' placeholder='Add Number' onChange={(e)=>setNumber(e.target.value)}/>
+      <button onClick={HandleCount}>Add By Input</button>
       <h1>Count : {count}</h1>
-      <button onClick={()=>dispatch(Add(10))}>Add</button>
-      <button onClick={()=>dispatch(Remove(5))}>Remove</button>
-      <button onClick={()=>dispatch(Multi(5))}>Multipal</button>
+      <button onClick={()=>dispatch(Add(10))}>Add-10</button>
+      <button onClick={()=>dispatch(Remove(5))}>Remove-5</button>
+      <button onClick={()=>dispatch(Multi(5))}>Multipal-5</button>
 
 
 
